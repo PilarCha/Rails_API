4a3485dad11d7482ba8ccf1712118ffa7772c1ee
PilarCha <pchavez92@gmail.com>
2021-10-27 19:35:22 -0700

    starting to work on the controller to CRUD our database

--- app/controllers/secret_menu_items_controller.rb
+++ app/controllers/secret_menu_items_controller.rb
@@ -1,2 +1,18 @@
 class SecretMenuItemsController < ApplicationController
+    def index
+        @secretMenuItems = SecretMenuItem.all
+        render json: @secretMenuItems
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
