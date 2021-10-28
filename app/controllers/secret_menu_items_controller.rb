62f653db00ae3056a30f0764e355f3333f839b6c
PilarCha <pchavez92@gmail.com>
2021-10-27 19:38:52 -0700

    finished controller CRUD actions

--- app/controllers/secret_menu_items_controller.rb
+++ app/controllers/secret_menu_items_controller.rb
@@ -5,14 +5,33 @@ class SecretMenuItemsController < ApplicationController
     end
 
     def show
+        @secretMenuItem = SecretMenuItem.find(params[:id])
+        render json: @secretMenuItem
     end
 
     def create
+        @secretMenuItem = SecretMenuItem.create(
+            menu_name: params[:menu_name],
+            restaurant_name: params[:restaurant_name],
+            menu_description: params[:menu_description]
+        )
+        render json: @secretMenuItem
     end
 
     def update
+        @secretMenuItem = SecretMenuItem.find(params[:id])
+        @secretMenuItem.update(
+            menu_name: params[:menu_name],
+            restaurant_name: params[:restaurant_name]
+            menu_description: params[:menu_description]
+        )
+        render json: @secretMenuItem
     end
 
     def destroy
+        @secretMenuItems = SecretMenuItem.all
+        @secretMenuItem = SecretMenuItem.find(params[:id])
+        @secretMenuItem.destroy
+        render json: @secretMenuItems
     end
 end
